d81b9b9ca707d71e993959cccfef2b46f3268226
DiegoBorraz <diegob.avila@hotmail.com>
2024-05-22 20:36:43 -0300

    table removed

--- target/classes/db/migration/V1__CREATE_TABLE_BASIC.sql
+++ target/classes/db/migration/V1__CREATE_TABLE_BASIC.sql
@@ -4,10 +4,3 @@ CREATE TABLE IF NOT EXISTS public.users(
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS public.message(
-  id UUID PRIMARY KEY NOT NULL,
-  id_sender UUID REFERENCES public.users(id),
-  id_intended UUID REFERENCES public.users(id),
-  content TEXT NOT NULL,
-  date_time TIMESTAMP WITH TIME ZONE NOT NULL
-);
